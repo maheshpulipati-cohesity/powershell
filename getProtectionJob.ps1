684b48fcc0d1d231b7231c56777b8154b54d17e7
sre-india <54129277+sre-india@users.noreply.github.com>
2019-12-05 00:13:40 +0530

    Add files via upload

---
+++ getProtectionJob.ps1
@@ -0,0 +1,20 @@
+### usage: ./getProtectionJob.ps1 -vip mycluster -username admin
+
+### process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True)][string]$vip, #Cohesity cluster to connect to
+    [Parameter(Mandatory = $True)][string]$username, #Cohesity username
+    [Parameter()][string]$domain = 'local' #Cohesity user domain name
+)
+
+
+### source the cohesity-api helper code
+. ./cohesity-api
+
+### authenticate
+apiauth -vip $vip -username $username -domain $domain
+
+### get existing protection jobs
+
+api get protectionJobs?names | grep name | cut -d ":" -f 2 | sed -e 's/^[ \t]*//'
